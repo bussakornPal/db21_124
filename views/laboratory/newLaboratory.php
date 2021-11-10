efe52a7080957910b68536a2dfb5804641c00a90
bussakornPal <bussakorn.pal@ku.th>
2021-11-10 16:56:50 +0700

    Update newLaboratory.php

--- views/laboratory/newLaboratory.php
+++ views/laboratory/newLaboratory.php
@@ -1,7 +1,7 @@
 <form method="get" action="">
 <label> รหัสห้องปฏิบัติการ <input type="text" name="id_lab"/> </label><br>
 <label> ชื่อห้องปฏิบัติการ <input type="text" name="name_lab"/> </label><br>
-<label> ที่อยู่ <input type="text" name="address_lab"/> </label><br>
+<label> ที่อยู่ <input type="text" name="address_lab"/> </label><br><br>
 
 <input type="hidden" name="controller" value = "laboratory"/>
 <button type="submit"name="action" value = "index"> back </button>
